cccdb13d81a06a7a6b2ed9e81b6f28f46347ac91
Craig Dennis <craigmdennis@gmail.com>
2020-03-27 10:38:32 +0000

    Initial working demo plugin

---
+++ src/code.ts
@@ -0,0 +1,21 @@
+import clone from './clone'
+
+figma.showUI(__html__)
+
+figma.ui.onmessage = msg => {
+  if (msg.type === 'generate') {
+    const nodes = []
+    
+
+    for (let i = 0; i < msg.count; i++) {
+      const rect = figma.createRectangle()
+      rect.x = i * 150
+      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
+      figma.currentPage.appendChild(rect)
+      nodes.push(rect)
+    }
+
+    figma.currentPage.selection = nodes
+    figma.viewport.scrollAndZoomIntoView(nodes)
+  }
+}
