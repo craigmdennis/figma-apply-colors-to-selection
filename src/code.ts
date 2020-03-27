b9163d0f18810be40b0da3f3a3ffbeeebaf3dfe7
Craig Dennis <craigmdennis@gmail.com>
2020-03-27 15:48:57 +0000

    Pass array of colors from UI to plugin code

--- src/code.ts
+++ src/code.ts
@@ -5,17 +5,19 @@ figma.showUI(__html__)
 figma.ui.onmessage = msg => {
   if (msg.type === 'generate') {
     const nodes = []
+
+    console.log(msg.colors);
     
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle()
-      rect.x = i * 150
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
-      figma.currentPage.appendChild(rect)
-      nodes.push(rect)
-    }
+  // for (let i = 0; i < msg.count; i++) {
+    // const rect = figma.createRectangle()
+    // rect.x = i * 150
+    // rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
+    // figma.currentPage.appendChild(rect)
+    // nodes.push(rect)
+  // }
 
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
+    figma.currentPage.selection = nodes;
+    // figma.viewport.scrollAndZoomIntoView(nodes)
   }
 }
--- src/ui.ts
+++ src/ui.ts
@@ -1,11 +1,25 @@
 import './ui.css'
 
-document.getElementById('create').onclick = () => {
-  const textbox = document.getElementById('count') as HTMLInputElement
-  const count = parseInt(textbox.value, 10)
-  parent.postMessage({ pluginMessage: { type: 'generate', count } }, '*')
-}
+document.getElementById('generate').onclick = () => {
 
-document.getElementById('cancel').onclick = () => {
-  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+  let nodeList : NodeList = document.querySelectorAll('.color');
+  let elementList : Array<HTMLInputElement> = [];
+  let colors : Array<String> = [];
+
+  if (nodeList) {
+    for (let i = 0; i < nodeList.length; i++) {
+      let node : Node = nodeList[i];
+
+      // Make sure it's really an Element
+      if (node.nodeType == Node.ELEMENT_NODE) {
+        elementList.push(node as HTMLInputElement);
+      }
+    }
+  }
+
+  elementList.forEach(element => {
+    colors.push(element.value);
+  });
+
+  parent.postMessage({ pluginMessage: {type: 'generate', colors }}, '*');
 }
